7647797d75a219fcd4862659b7619503631692a0
georgegarrott93 <p15240691@my365.dmu.ac.uk>
2017-11-22 13:47:01 +0000

    First Index draft page

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php
+$eol = "\n";
+$output = '<!DOCTYPE html >' . $eol;
+$output .= '<head lang="en">' . $eol;
+$output .= '<meta httpequiv="
+ContentType"
+content="text/html;
+charset=utf8"
+/>' . $eol;
+$output .= '<meta name="Author" content="George" />' . $eol;
+$output .= '<title>Circuit Statistics</title>' . $eol;
+$output .= '</head>' . $eol;
+$output .= '<body>' . $eol;
+$output .= '<h2>Hello World</h2>' . $eol;
+$output .= '<p>This is a draft of the index webpage</p>' . $eol;
+$output .= '</body>' . $eol;
+$output .= '</html>' . $eol;
+echo $output;
